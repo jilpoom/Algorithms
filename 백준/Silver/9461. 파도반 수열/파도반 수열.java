6182c97012b7eb005d3a0f85e363b3c460fccf96
정정용 <64225078+jilpoom@users.noreply.github.com>
2022-12-25 21:14:19 +0900

    [Silver III] Title: 파도반 수열, Time: 128 ms, Memory: 13236 KB -BaekjoonHub

---
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class Main {
+
+    static long[] waveSequenceArray = new long[101];
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int testCase = sc.nextInt();
+
+        for(int i = 0; i < testCase; i++){
+            int input = sc.nextInt();
+
+            System.out.println(waveSequence(input));
+        }
+
+
+
+    }
+
+
+    public static long waveSequence(int i){
+       long[] waveArray = new long[100];
+       waveSequenceArray[0] = 1L;
+       waveSequenceArray[1] = 1L;
+       waveSequenceArray[2] = 1L;
+
+       if(i <= 3 || waveSequenceArray[i - 1] != 0){
+           return waveSequenceArray[i - 1];
+        } else {
+           waveSequenceArray[i - 1] = waveSequence(i - 2) + waveSequence(i - 3);
+           return waveSequenceArray[i - 1];
+       }
+
+    }
+
+
+
+
+}

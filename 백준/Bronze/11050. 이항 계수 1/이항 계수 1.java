070151c4f96654417547f48b5c896b6d55000bd3
정정용 <64225078+jilpoom@users.noreply.github.com>
2023-01-05 22:58:56 +0900

    [Bronze I] Title: 이항 계수 1, Time: 76 ms, Memory: 11484 KB -BaekjoonHub

---
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.java
@@ -0,0 +1,49 @@
+import com.sun.java.swing.plaf.windows.WindowsTextAreaUI;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    /**
+     * 2023-01-05
+     *
+     * @author JY
+     * https://www.acmicpc.net/problem/11050
+     */
+
+
+    static int[][] dp;
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        dp = new int[N + 1][K + 1];
+
+        System.out.println(BC(N, K));
+
+    }
+
+    static int BC(int n, int k) {
+
+        // 이미 풀었던 sub문제일 경우 값을 재활용
+        if (dp[n][k] > 0) {
+            return dp[n][k];
+        }
+
+        // 2번 성질
+        if (k == 0 || n == k) {
+            return dp[n][k] = 1;
+        }
+
+        // 1번 성질
+        return dp[n][k] = BC(n - 1, k - 1) + BC(n - 1, k);
+    }
+
+
+}

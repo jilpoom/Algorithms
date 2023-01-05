c2cf34085b115b7e62a89c5ba8408ae81a66658e
정정용 <64225078+jilpoom@users.noreply.github.com>
2023-01-05 18:26:19 +0900

    [Bronze V] Title: 그대로 출력하기, Time: 84 ms, Memory: 12100 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    /**
+     * 2023-01-05
+     *
+     * @author JY
+     * https://www.acmicpc.net/problem/11718
+     * */
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String result;
+
+        while((result = br.readLine()) != null){
+            System.out.println(result);
+        }
+
+    }
+
+
+
+}

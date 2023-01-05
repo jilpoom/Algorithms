8899e49bde989afe867311083cd8a552b22d0256
정정용 <64225078+jilpoom@users.noreply.github.com>
2023-01-05 16:58:20 +0900

    [Bronze V] Title: 새싹, Time: 72 ms, Memory: 11228 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    /**
+     * 2023-01-05
+     *
+     * @author JY
+     * https://www.acmicpc.net/problem/25083
+     */
+
+
+
+    public static void main(String[] args) {
+
+        String result = "         ,r'\"7\n" +
+                "r`-_   ,'  ,/\n" +
+                " \\. \". L_r'\n" +
+                "   `~\\/\n" +
+                "      |\n" +
+                "      |";
+
+        System.out.println(result);
+
+    }
+
+
+
+}

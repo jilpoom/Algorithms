9098cf2aea67915341fd4b07472e393a1b53bfe0
정정용 <64225078+jilpoom@users.noreply.github.com>
2023-01-05 16:56:03 +0900

    [Bronze V] Title: 등록, Time: 72 ms, Memory: 11204 KB -BaekjoonHub

---
+++ 백준/Bronze/7287. 등록/등록.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    /**
+     * 2023-01-05
+     *
+     * @author JY
+     * https://www.acmicpc.net/problem/7287
+     */
+
+    
+
+    public static void main(String[] args) throws IOException {
+
+        System.out.println("105");
+        System.out.println("jilpoom");
+
+    }
+    
+  
+
+}

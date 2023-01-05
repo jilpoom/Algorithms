7dbb35fa4a0a8848524efe7509f506829d5d78f7
정정용 <64225078+jilpoom@users.noreply.github.com>
2023-01-05 17:47:40 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 76 ms, Memory: 11464 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    /**
+     * 2023-01-05
+     *
+     * @author JY
+     * https://www.acmicpc.net/problem/2743
+     */
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+
+        String line = bf.readLine();
+
+        System.out.println(line.length());
+
+    }
+
+
+
+}

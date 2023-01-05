77d90b994ff4fe031ccce74718ae563b101b511c
정정용 <64225078+jilpoom@users.noreply.github.com>
2023-01-05 21:34:17 +0900

    [Bronze I] Title: 최대공약수와 최소공배수, Time: 112 ms, Memory: 12844 KB -BaekjoonHub

---
+++ 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    /**
+     * 2023-01-05
+     * 유클리드 호제법
+     * @author JY
+     * https://www.acmicpc.net/problem/2609
+     * */
+
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+
+        int a = in.nextInt();
+        int b = in.nextInt();
+
+        int d = gcd(a, b);	// 최대공약수
+
+        System.out.println(d);
+        System.out.println(a * b / d); // 최소공배수
+
+    }
+
+    // 최대공약수 재귀 방식
+    public static int gcd(int a, int b) {
+        if (b == 0)
+            return a;
+
+        // GCD(a, b) = GCD(b, r)이므로 (r = a % b)
+        return gcd(b, a % b);
+    }
+
+}
+    

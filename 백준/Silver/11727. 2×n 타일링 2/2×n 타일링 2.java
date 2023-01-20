392c43ead9c8020c74fb1b55b10eafdc11c9b598
정정용 <64225078+jilpoom@users.noreply.github.com>
2023-01-20 15:58:05 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 108 ms, Memory: 12852 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.io.*;
+
+
+public class Main {
+    /**
+     * Dynamic Programming
+     * 2023-01-20
+     *
+     * @author JY
+     * https://www.acmicpc.net/problem/11726
+     */
+
+
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] DP = new int[n + 2];
+        DP[1] = 1;
+        DP[2] = 3;
+        for (int i = 3; i <= n; i++) {
+            DP[i] = (DP[i - 1] + (2 * DP[i - 2])) % 10007;
+        }
+        System.out.println(DP[n] % 10007);
+
+
+    }
+
+}

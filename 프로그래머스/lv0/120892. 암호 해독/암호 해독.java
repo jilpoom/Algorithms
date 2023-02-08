b316ab4108a17a7315c1b05015fc2474bdd97af8
Palladium <64225078+jilpoom@users.noreply.github.com>
2023-02-08 13:05:34 +0900

    [level 0] Title: 암호 해독, Time: 2.04 ms, Memory: 76 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120892. 암호 해독/암호 해독.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String solution(String cipher, int code) {
+        String answer = "";
+        String[] cipherArr = cipher.split("");
+        
+        for(int i = code - 1; i < cipherArr.length; i += code) {
+            answer += cipherArr[i];
+        }        
+        
+        
+        return answer;
+    }
+}

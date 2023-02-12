e4527a554eeaf7138ffce5dc4961d8e91edcec64
Palladium <64225078+jilpoom@users.noreply.github.com>
2023-02-13 05:14:12 +0900

    [level 0] Title: 캐릭터의 좌표, Time: 0.02 ms, Memory: 76.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120861. 캐릭터의 좌표/캐릭터의 좌표.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] solution(String[] keyinput, int[] board) {
+        int[] answer = {0, 0};
+        
+        for(int i = 0; i < keyinput.length; i++) {
+            if(keyinput[i].equals("up") && answer[1] != board[1] / 2) answer[1]++;
+            if(keyinput[i].equals("down") && answer[1] != -(board[1] / 2)) answer[1]--;
+            if(keyinput[i].equals("right") && answer[0] != board[0] / 2) answer[0]++;
+            if(keyinput[i].equals("left") && answer[0] != -(board[0] / 2)) answer[0]--;
+        }
+        
+        
+        
+        return answer;
+    }
+}

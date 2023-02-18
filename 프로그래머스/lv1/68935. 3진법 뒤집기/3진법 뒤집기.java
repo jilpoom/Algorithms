5d51bab7c17a49e169faf1577c8586df823c06a1
Palladium <64225078+jilpoom@users.noreply.github.com>
2023-02-19 03:11:06 +0900

    [level 1] Title: 3진법 뒤집기, Time: 8.58 ms, Memory: 79.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Solution {
+    public int solution(int n) {
+        
+        int answer = 0;
+        
+        String ans = "";
+        
+        while(n != 0) {
+            ans += n % 3;
+            n /= 3;
+        }
+        
+        return Integer.parseInt(ans, 3);
+    }
+}

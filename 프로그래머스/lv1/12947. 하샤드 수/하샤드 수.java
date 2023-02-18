d311653b35ab6d554da418f33ff23910d3f6b0b4
Palladium <64225078+jilpoom@users.noreply.github.com>
2023-02-18 23:15:34 +0900

    [level 1] Title: 하샤드 수, Time: 1.25 ms, Memory: 72.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+class Solution {
+    public boolean solution(int x) {
+        int sum = Arrays.stream(String.valueOf(x).split(""))
+            .mapToInt(Integer::parseInt)
+            .sum();
+        return x % sum == 0 ? true : false;
+    }
+}

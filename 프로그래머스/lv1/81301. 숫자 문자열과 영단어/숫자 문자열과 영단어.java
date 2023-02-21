78f2bffb22d4fb2e217c007310123e0965df0af3
Palladium <64225078+jilpoom@users.noreply.github.com>
2023-02-21 09:25:24 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.29 ms, Memory: 80.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
@@ -0,0 +1,12 @@
+class Solution {
+    private static String[] numbers = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+    
+    public int solution(String s) {
+        
+        for(int i = 0; i < numbers.length; i++) {
+            s = s.replaceAll(numbers[i], String.valueOf(i));
+        }
+        
+        return Integer.parseInt(s);
+    }
+}

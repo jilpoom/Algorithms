5c3515b7cb08f9eb826dbd0888d47e5e9836a43f
Palladium <64225078+jilpoom@users.noreply.github.com>
2023-02-24 18:28:24 +0900

    [Bronze II] Title: 알고리즘 수업 - 알고리즘의 수행 시간 6, Time: 120 ms, Memory: 12864 KB -BaekjoonHub

---
+++ 백준/Bronze/24267. 알고리즘 수업 － 알고리즘의 수행 시간 6/알고리즘 수업 － 알고리즘의 수행 시간 6.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Main {
+    /**
+     * Time Complexity
+     * 2023-02-24
+     *
+     * @author JY
+     * https://www.acmicpc.net/problem/24267
+     */
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long n = sc.nextInt();
+
+        long sum = 0;
+
+        for(long i = n - 2; i >= 1; i--) {
+            sum +=  i * (i + 1) / 2;
+        }
+
+
+        System.out.println(sum);
+        System.out.print(3);
+    }
+
+}

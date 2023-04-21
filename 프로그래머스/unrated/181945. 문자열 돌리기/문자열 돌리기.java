f100a05aee1e205004f2bda46dabddd511f33add
Palladium <64225078+jilpoom@users.noreply.github.com>
2023-04-22 04:58:55 +0900

    [unrated] Title: 문자열 돌리기, Time: 135.21 ms, Memory: 73.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181945. 문자열 돌리기/문자열 돌리기.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        StringBuilder sb = new StringBuilder();
+        for(char c : a.toCharArray()) sb.append(c + "\n");
+        
+        System.out.println(sb);
+    }
+}

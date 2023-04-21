ceae1f45bca89f3e28581c6ba5e4e41853dd7ec9
Palladium <64225078+jilpoom@users.noreply.github.com>
2023-04-22 04:51:10 +0900

    [unrated] Title: 배열 만들기 1, Time: 6.16 ms, Memory: 80.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181901. 배열 만들기 1/배열 만들기 1.java
@@ -0,0 +1,8 @@
+import java.util.*;
+import java.util.stream.IntStream;
+
+class Solution {
+    public int[] solution(int n, int k) {
+        return IntStream.rangeClosed(1, n).filter(i -> i % k == 0).toArray();
+    }
+}

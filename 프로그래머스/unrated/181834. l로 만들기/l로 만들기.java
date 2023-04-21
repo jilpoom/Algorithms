a1092022b9d0488c0281c6ab80fd41ca13ec6ee9
Palladium <64225078+jilpoom@users.noreply.github.com>
2023-04-22 08:46:46 +0900

    [unrated] Title: l로 만들기, Time: 617.51 ms, Memory: 391 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181834. l로 만들기/l로 만들기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String solution(String myString) {
+        String answer = "";
+        
+        for(char c : myString.toCharArray()) {
+            answer += c < 'l' ? 'l' : c;
+        }
+        
+        return answer;
+    }
+}

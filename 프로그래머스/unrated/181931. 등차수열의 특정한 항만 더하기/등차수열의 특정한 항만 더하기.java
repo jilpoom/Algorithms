cd62d15f13f47865c9be78c36a119f04a4a6e89d
Palladium <64225078+jilpoom@users.noreply.github.com>
2023-05-08 22:06:04 +0900

    [unrated] Title: 등차수열의 특정한 항만 더하기, Time: 2.51 ms, Memory: 79.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181931. 등차수열의 특정한 항만 더하기/등차수열의 특정한 항만 더하기.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.util.stream.*;
+
+class Solution {
+    public int solution(int a, int d, boolean[] included) {
+        return IntStream.range(0, included.length)
+            .filter(i -> included[i])
+            .map(i -> a + i * d)
+            .sum();
+    }
+}

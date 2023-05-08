a3d26c1177db4abcf5c6d87ac8f5a27643238d6c
Palladium <64225078+jilpoom@users.noreply.github.com>
2023-05-09 03:17:18 +0900

    [unrated] Title: 수열과 구간 쿼리 3, Time: 0.03 ms, Memory: 74.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+class Solution {
+    public int[] solution(int[] arr, int[][] queries) {
+        for(int[] query : queries) {
+            int tmp = arr[query[0]];
+            arr[query[0]] = arr[query[1]];
+            arr[query[1]] = tmp;
+        }
+        
+        return arr;
+    }
+}

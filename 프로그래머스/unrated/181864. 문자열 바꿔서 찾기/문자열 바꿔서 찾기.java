0974f86b7ce2568cf7683ac5403178aa07309b74
Palladium <64225078+jilpoom@users.noreply.github.com>
2023-09-16 02:23:14 +0900

    [unrated] Title: 문자열 바꿔서 찾기, Time: 1.73 ms, Memory: 75.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.java
+++ 프로그래머스/unrated/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.java
@@ -3,7 +3,12 @@ import java.util.stream.*;
 
 class Solution {
     public int solution(String myString, String pat) {
+        
         return myString.contains(Arrays.stream(pat.split(""))
-            .map(s -> s.equals("A") ? "B" : "A").collect(Collectors.joining(""))) ? 1 : 0;
+            .map(s ->
+                
+                s.equals("A") ? "B" : "A"
+
+                 ).collect(Collectors.joining(""))) ? 1 : 0;
     }
 }

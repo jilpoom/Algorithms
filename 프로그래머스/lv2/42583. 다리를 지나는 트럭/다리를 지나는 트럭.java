803c221df887bb914ae834cc0886a9468e6b12fe
Palladium <64225078+jilpoom@users.noreply.github.com>
2023-09-15 21:28:24 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 0.06 ms, Memory: 78.3 MB -BaekjoonHub

--- 프로그래머스/lv2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
+++ 프로그래머스/lv2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
@@ -8,7 +8,7 @@ class Solution {
     
     public int solution(int bridge_length, int weight, int[] truck_weights) {    
         
-        // 1. 다리로 이동
+        // truck_weights 배열의 마지막까지 다리로 이동.
         for(int i = 0; i < truck_weights.length; i++) {
             curWeight += truck_weights[i];
             
@@ -16,7 +16,6 @@ class Solution {
                 curWeight -= truck_weights[i];
                 i--;
                 queue.offer(0);
-
             } else {
                 queue.offer(truck_weights[i]);
             }
@@ -24,13 +23,11 @@ class Solution {
             if(queue.size() == bridge_length) {
                 curWeight -= queue.poll();
             } 
-
             answer++;
-            
         }
         
-
-        
+        // 지나갈 수 없는 자리에는 0을 채웠으므로, 완전히 queue(다리)를 비우기 위해서는 
+        // 이미 지난 시간에, queue가 빌 때까지의 시간(bridge_length)를 더해준다.
         return answer + bridge_length; 
     }
 }
